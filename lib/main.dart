462ccb13082665eb43edb89b28b12fccf9d586d3
yang zhang <tianna1121@gmail.com>
2019-09-11 15:17:51 +0800

    add CustomListTile class to design the ListTile.

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,91 @@ class HomeScreen extends StatelessWidget {
         title: Text('Drawer Demo'),
         backgroundColor: Colors.deepOrange,
       ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Material(
+                      elevation: 10.0,
+                      borderRadius: BorderRadius.circular(50.0),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.asset(
+                          'assets/images/logo.png',
+                          width: 80,
+                          height: 80,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Flutter',
+                        style: TextStyle(color: Colors.white, fontSize: 20.0),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: <Color>[Colors.deepOrange, Colors.orangeAccent])),
+            ),
+            CustomListTile(Icons.person, 'Profile', () => {}),
+            CustomListTile(Icons.notifications, 'Notification', () => {}),
+            CustomListTile(Icons.settings, 'Setting', () => {}),
+            CustomListTile(Icons.lock, 'Log out', () => {}),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomListTile extends StatelessWidget {
+  IconData icon;
+  String text;
+  Function onTap;
+  CustomListTile(this.icon, this.text, this.onTap);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border(bottom: BorderSide(color: Colors.grey.shade400))),
+        child: InkWell(
+          splashColor: Colors.orangeAccent,
+          onTap: () {
+            onTap();
+          },
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                height: 40.0,
+                child: Row(
+                  children: <Widget>[
+                    Icon(icon),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        text,
+                        style: TextStyle(fontSize: 16.0),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Icon(Icons.arrow_right)
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
